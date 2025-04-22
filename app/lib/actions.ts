427e8bc795eed88b87e7a9e1d1925250f85ddd3a
oigomezz <oigomezz@unal.edu.co>
2025-04-22 09:30:36 -0500

    ✨ Se implementa autenticación de usuarios con manejo de errores y redirección; se añade página de inicio de sesión y se refactoriza la lógica de autenticación.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@
 import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 import postgres from "postgres";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
@@ -108,4 +110,20 @@ export async function updateInvoice(
 export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      const errorType = (error as any).type;
+      if (errorType === "CredentialsSignin") return "Invalid credentials.";
+      else return "Something went wrong.";
+    }
+    throw error;
+  }
 }
